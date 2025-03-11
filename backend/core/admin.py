172a0cc5d2607f80da53ea1f3a16c11aa97330ae
Candle1102 <oussama4448@gmail.com>
2025-03-11 16:35:44 +0100

    11-03 / 16h30 avec le smodif de gantt et sql fonctionne pas

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from core.models import Gantt, GanttStatistics  # Utilisez le bon nom de modèle
+from core.models import ARD2
 
 # Enregistrez les modèles dans l'interface d'administration
 admin.site.register(Gantt)
 admin.site.register(GanttStatistics)
+
+@admin.register(ARD2)
+class ARD2Admin(admin.ModelAdmin):
+    list_display = ('jeton_commande', 'technicien', 'departement', 'etat_intervention', 'terminee', 'date_importation')
+    list_filter = ('etat_intervention', 'terminee', 'departement')
+    search_fields = ('jeton_commande', 'technicien')
